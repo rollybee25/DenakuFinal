2c40f45de8ef0d628e66cea0abae0e47bf71629a
rollybee25 <rollybee25@gmail.com>
2022-04-06 16:57:11 +0800

    second

---
+++ public/js/productCss.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('.content-wrapper').css("height", "");
+});
--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -3,6 +3,44 @@
 @section('title', 'Products')
 
 <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+<style>
+
+    .grid-container {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      padding: 10px;
+      grid-gap: 10px;
+    }
+
+
+    .product-card {
+      background-color: rgba(255, 255, 255, 0.8);
+      padding: 20px;
+      min-width: 200px;
+      text-align: center;
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+    }
+
+    @media screen and (max-width: 1200px) {
+          .grid-container {
+                grid-template-columns: 1fr 1fr 1fr;
+          }
+    }
+
+    @media screen and (max-width: 750px) {
+          .grid-container {
+                grid-template-columns: 1fr 1fr;
+          }
+    }
+
+    @media screen and (max-width: 450px) {
+          .grid-container {
+                grid-template-columns: 1fr;
+          }
+    }
+
+    
+</style>
 
 @section('content')
 
@@ -13,21 +51,24 @@
       <div class="row mb-2">
         <button type="button" class="btn-size save-btn" data-toggle="modal"  data-target="#add_product_modal" role="button">Add New Product</button>
       </div><!-- /.row -->
-      @foreach ($products as $product)
-          <div class="product-card col-md-3">
-              <div class="product-header">
-                  <h1>{{ $product->name }}</h1>
-                  <span><i class="fas fa-brands fa-times"></i></i></span>
-              </div>
-              <div class="product-details">
+
+      <div class="grid-container">
+            @foreach ($products as $product)
+            <div class="product-card">
+                <div class="product-header">
+                    <h1>{{ $product->name }}</h1>
+                </div>
+                <div class="product-stocks">
+                  <h2>{{ $product->stocks }}</h2>
+                  <p>Available Stocks</p>
+                </div>
+                <div class="product-details">
                   <p>{{ $product->details }}</p>
-              </div>
-              <div class="product-stocks">
-                <h2>{{ $product->stocks }}</h2>
-                <p>Available Stocks</p>
+                </div>
             </div>
-          </div>
-      @endforeach
+          @endforeach
+      </div>
+      
       
 
       
@@ -51,12 +92,24 @@
 
 @section('scripts')
   <script>
+
+      //Add some event listener to manipulate CSS
+
+      const wrapper = document.querySelector('.content-wrapper');
+
+      window.addEventListener('resize', function(event){
+          wrapper.style.backgroundColor = "#f4f6f9";
+      });
+      
+
       $(document).ready(function(){
           $.ajaxSetup({
               headers: {
                   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
               }
           });
+
+          $('.content-wrapper').css("height", "auto");
 
           $(document).on('click', '#product_save', function(e){
               e.preventDefault();
